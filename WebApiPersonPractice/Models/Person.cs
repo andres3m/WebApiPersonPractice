7312f569fe32ded8d0f07091beb6e73a22141de5
andres3m <andresmartinezuk@gmail.com>
2024-09-20 17:24:25 +0100

    API implemented

---
+++ WebApiPersonPractice/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiPersonPractice.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Age { get; set; }
+    }
+}
--- WebApiPersonPractice/Program.cs
+++ WebApiPersonPractice/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiPersonPractice.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Variable for conexion string
+var connectionString = builder.Configuration.GetConnectionString("Connection");
+// Connexion service registration
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
